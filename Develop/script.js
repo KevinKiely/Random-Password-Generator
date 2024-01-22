d1904545a84591905cab57da25db2608748f53da
KevinKiely <Kevinkiely762@gmail.com>
2024-01-22 14:49:32 -0500

    Initial Commit, added selectors for password length, Upper/Lower/Special character inclusion, slight modifications to CSS, added arrays for characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,40 @@
-// Assignment code here
-
 
 // Get references to the #generate element
+
+/*
 var generateBtn = document.querySelector("#generate");
+var passLength = document.querySelector('#length');
+var passUpper = document.querySelector('#Upper');
+var passLower = document.querySelector('#Lower');
+var passNumbers = document.querySelector('#Numbers');
+var passSpecial = document.querySelector('#Special');
+*/
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCaseArray = upperCase.split("");
+var lowerCase = "abcdefghijklmnopqrstuvwyz";
+var lowerCaseArray = lowerCase.split("");
+var numbers = "0123456789";
+var numbersArray = numbers.split("");
+var special = "!@#$%^&*()_+=<>?/";
+var specialArray = special.split("");
+
+
+/* let length = passLength.value;  */
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
+
+  ar passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
